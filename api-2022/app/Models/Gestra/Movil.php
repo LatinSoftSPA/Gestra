867ea625a33a6af5b0a6ea0a28a530c1b24e288f
LatinSoftSPA <latinsoft.chile@gmail.com>
2022-09-12 11:37:26 -0300

    UPDATE Modelos...!!!

---
+++ api-2022/app/Models/Gestra/Movil.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Gestra;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movil extends Model
+{
+    use HasFactory;
+}
